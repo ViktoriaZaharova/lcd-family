a664207255d4a3c9e007ccac27895f61f535fd3e
Viktoria <zaharova181114@gmail.com>
2022-03-30 01:08:48 +0300

    add media

--- app/js/common.js
+++ app/js/common.js
@@ -6,7 +6,17 @@ $('.family-slider').slick({
     appendArrows: '.family-slider__nav',
     appendDots: '.family-slider__nav',
     prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
-    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>'
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+    responsive: [
+        {
+            breakpoint: 576,
+            settings: {
+                appendArrows: '.family-slider',
+                dots: true,
+            }
+        }
+
+    ]
 });
 
 $('.about-complex-slider').slick({
@@ -30,4 +40,115 @@ $('.about-complex-nav').slick({
     infinite: false,
     focusOnSelect: true,
     asNavFor: '.about-complex-slider, .about-complex-text-slider',
+});
+
+$('.planes-gallery').slick({
+    slidesToShow: 2,
+    prevArrow: '<button type="button" class="slick-prev slick-border"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next slick-border"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+    responsive: [
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+
+    ]
+});
+
+
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+    $('.slick-slider').slick('setPosition');
+});
+
+$('.months-slider').slick({
+    slidesToShow: 12,
+    asNavFor: '.stages-work-slider',
+    arrows: false,
+    variableWidth: true,
+    focusOnSelect: true,
+    infinite: false
+});
+
+$('.stages-work-slider').slick({
+    slidesToShow: 2,
+    prevArrow: '<button type="button" class="slick-prev slick-border"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next slick-border"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+    appendArrows: '.stages-work-slider__nav',
+    responsive: [
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+
+    ]
+});
+
+
+//плавный скролл
+$(document).ready(function () {
+    $('.go_to').click(function (e) {
+        e.preventDefault();
+        var scroll_el = $(this).attr('href');
+        if ($(scroll_el).length !== 0) {
+            $('html, body').animate({
+                scrollTop: $(scroll_el).offset().top
+            }, 1000);
+        }
+
+        $('.mobile-menu').fadeOut();
+
+        return false;
+    });
+});
+//плавный скролл end
+
+let homeSlider = $('.stages-work-slider');
+
+$('.counter-slide__default').text("из " + homeSlider.slick("getSlick").slideCount);
+
+homeSlider.on('afterChange', function(event, slick, currentSlide){
+    $(".counter-slide__cp").text(currentSlide<10?`${currentSlide+1}`:currentSlide+1);
+});
+
+$('[name="phone"]').mask('+7(999) 999-99-99');
+
+// slick active
+$(window).on('load resize', function() {
+    if ($(window).width() < 768) {
+        $('.documents-slider:not(.slick-initialized)').slick({
+            dots: false,
+            infinite: true,
+            slidesToShow: 2,
+            arrows: false,
+            variableWidth: true,
+        });
+    } else {
+        $(".documents-slider.slick-initialized").slick("unslick");
+    }
+    if ($(window).width() < 576) {
+        $('.advantages-slider:not(.slick-initialized)').slick({
+            dots: false,
+            infinite: true,
+            slidesToShow: 2,
+            arrows: false,
+            variableWidth: true,
+        });
+    } else {
+        $(".advantages-slider.slick-initialized").slick("unslick");
+    }
+});
+// slick active
+
+$('.btn-burger').click(function (e) {
+    e.preventDefault();
+    $('.mobile-menu').fadeToggle();
+});
+
+$('.btn-close').click(function (e) {
+    e.preventDefault();
+    $('.mobile-menu').fadeOut();
 });
